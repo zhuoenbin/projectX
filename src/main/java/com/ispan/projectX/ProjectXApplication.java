63afee991c6834b9baf7ff283aae8a4e857ba654
Ruan <a50064xxx@gmail.com>
2024-03-07 14:42:39 +0800

    做employee

--- src/main/java/com/ispan/projectX/ProjectXApplication.java
+++ src/main/java/com/ispan/projectX/ProjectXApplication.java
@@ -1,7 +1,13 @@
 package com.ispan.projectX;
 
+import com.ispan.projectX.dao.EmployeeRepository;
+import com.ispan.projectX.entity.Employee;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ProjectXApplication {
@@ -9,5 +15,24 @@ public class ProjectXApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectXApplication.class, args);
 	}
+
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(EmployeeRepository employeeRepository){
+		return runner -> {
+			findAllEmployee(employeeRepository);
+
+		};
+	}
+
+	private void findAllEmployee(EmployeeRepository employeeRepository) {
+		List<Employee> lis = employeeRepository.findAll();
+		for(Employee li : lis){
+			System.out.println(li.toString());
+		}
+	}
+
+
 
 }
