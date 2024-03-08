2ee48c5026c2ffbfa8c8631ff6722b1ef4858dc7
enbin <boy820408@gmail.com>
2024-03-08 09:15:04 +0800

    no message

--- src/main/java/com/ispan/projectX/ProjectXApplication.java
+++ src/main/java/com/ispan/projectX/ProjectXApplication.java
@@ -22,41 +22,6 @@ public class ProjectXApplication {
 	}
 
 
-
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ComplaintRepository complaintRepository,EmployeeRepository employeeRepository){
-		return runner -> {
-			findComplaintByEmployee(employeeRepository);
-        };
-	}
-
-
-
-	//找出該employee所處理的所有Complaint(By Employee物件)，使用employeeRepository
-	private void findComplaintByEmployee(EmployeeRepository employeeRepository){
-		Employee emp = employeeRepository.findByEmployeeId(2);
-		List<Complaint> lis = employeeRepository.findComplaintByEmployee(emp);
-		for (Complaint li : lis) {
-			System.out.println(li.toString());
-		}
-	}
-
-	//找出該employee所處理的所有Complaint(By EmployeeId)，使用complaintRepository
-	private  void findComplaintByEmployeeId(ComplaintRepository complaintRepository){
-		List<Complaint> lis = complaintRepository.findByEmployeeEmployeeId(2);
-		for(Complaint li:lis){
-			System.out.println(li.toString());
-		}
-	}
-
-	//在Complaint加入EmployeeId(讓他們有關係)
-	private void setEmployeeIdInComplaint(ComplaintRepository complaintRepository,EmployeeRepository employeeRepository){
-		Employee emp = employeeRepository.findByEmployeeId(1);
-		Complaint com = complaintRepository.findByComplaintId(1);
-		com.setEmployee(emp);
-		complaintRepository.save(com);
-	}
 	private void findAllEmployee(EmployeeRepository employeeRepository) {
 		List<Employee> lis = employeeRepository.findAll();
 		for(Employee li : lis){
